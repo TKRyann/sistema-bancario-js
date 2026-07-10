dffe56c8128a55031d356bfbf31629f2fb9b90c5
Kherlysson Ryann <106851981+TKRyann@users.noreply.github.com>
2026-07-09 22:49:39 -0300

    feat: Implementar função de depósito e atualizar lógica de opções

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 let saldo = 1000;
+
 let opcao = 0;
 
 function consultarSaldo() {
   return saldo;
 }
 
-function depositar() {}
+function depositar(saldo, depositoFeito) {
+  return saldo + depositoFeito;
+}
 
 function sacar() {}
 
@@ -17,13 +20,15 @@ Escolha uma opção:
 1 - Consultar saldo
 2 - Depositar
 3 - Sacar
-4 - Sair
-`),
+4 - Sair`),
   );
 
   if (opcao == 1) {
     alert(`Seu saldo disponivel é de R$${consultarSaldo()}`);
-  } else {
-    console.log("testeee");
+  } else if (opcao == 2) {
+    let depositoFeito = Number(prompt("Qual o valor deseja depositar?"));
+    saldo = depositar(saldo, depositoFeito);
+    alert(`Depósito realizado!
+      Novo saldo: R$ ${saldo} `);
   }
 }
