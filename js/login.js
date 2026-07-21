7eee425ed3a844f19fddd08cc9b6b15d835ce2b7
Kherlysson Ryann <106851981+TKRyann@users.noreply.github.com>
2026-07-21 10:18:39 -0300

    feat: implementar estilos e estrutura para a página de login

---
+++ js/login.js
@@ -0,0 +1,4 @@
+const formLogin = document.getElementById("form-login");
+const email = document.getElementsById("email");
+const senha = document.getElementById("senha");
+const checkboxMostraSenha = document.getElementById("checkbox-mostrar-senha");
